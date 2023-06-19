842890fbe4018f9bc7b62bc5148273ff84c33a6a
Oswaldo Montaño <oswaldoeliezer876@gmail.com>
2023-06-19 22:37:20 +0200

    ✨ Feat: Implement using the word2vect model API
        -  Implemented using the word2vect model API instead of the locally
           loaded model.
        -  Refactored get_simple_definitions and get_tables_related_to_the_question
           function to read text files generated by the db2definitions tool instead of
           reading a JSON file.
       -  .env.example file is added with an example of the environment variables
          needed to run the application.

--- microservice.py
+++ microservice.py
@@ -1,16 +1,10 @@
-import gensim.downloader as api
+import os
 from flask import Flask, jsonify, request
-from microservice_helper import get_simple_definitions, get_list_of_table_names, get_tables_related_to_the_question
+from dotenv import load_dotenv
+from microservice_helper import load_model, get_simple_definitions, get_list_of_table_names, get_tables_related_to_the_question
+
 
 app = Flask(__name__)
-
-# singelton pattern to load the model only once when the first request is received
-def load_model():
-    if not hasattr(app.config, 'modelo_word2vec'):
-        app.config['modelo_word2vec'] = api.load('word2vec-google-news-300')
-    return app.config['modelo_word2vec']
-
-
 
 # ping route
 @app.route('/ping')
@@ -26,7 +20,7 @@ def ping():
 def not_found(error):
     return {'success': False,
             'error': 'page not found',
-            'message': 'Ups, something went wrong :(, this page does not exist'}, 404
+            'message': 'Oops, something went wrong :(, this page does not exist'}, 404
 
 
 # get question route
@@ -35,21 +29,43 @@ def get_question():
     # get the question from the request body
     data = request.json
     question = data.get("question")
-    table, error = get_tables_related_to_the_question(question)
+    temperature = data.get("temperature")
+    
+    # check if the question is not None
+    if question is None:
+        return {'success': False,
+                'error': 'question not found',
+                'message': 'Oops, something went wrong :(, the question was not found'}, 400
+
+    table, error = get_tables_related_to_the_question(question, temperature)
+    
+    # check if there is an error
     if error is not None:
         return {'success': False,
                 'error': error,
-                'message': 'Ups, something went wrong :('}, 500
-    if table is None:
+                'message': 'Oops, something went wrong :('}, 500
+
+    # check if the table is empty
+    if len(table) == 0:
         return {'success': False,
                 'error': 'Empty list',
                 'message': 'No tables related to the question were found'}, 200
-    question = {'question': question, 'table': table}
     
+    definitions_of_tables = ""
+    for table_name in table:
+        print("ok")
+        definition, error = get_simple_definitions(table_name)
+        if error is not None:
+            return {'success': False,
+                    'error': error,
+                    'message': 'Oops, something went wrong :('}, 500
+        definitions_of_tables += definition + "\n"
+
     return {'success': True,
             'error': None,
             'message': 'success',
-            'data': question}, 200
+            'question': question,
+            'definitions': definitions_of_tables}, 200
 
 # get definition of table route by name
 @app.route('/definition/<table_name>', methods=['GET'])
@@ -58,7 +74,7 @@ def get_definition_by_name(table_name):
     if error is not None:
         return {'success': False,
                 'error': error,
-                'message': 'Ups, something went wrong :('}, 500
+                'message': 'Oops, something went wrong :('}, 500
     if len(definition) == 0:
         return {'success': False,
                 'error': 'Empty list',
@@ -76,17 +92,33 @@ def debug():
     if error is not None:
         return {'success': False,
                 'error': error,
-                'message': 'Ups, something went wrong :('}, 500
+                'message': 'Oops, something went wrong :('}, 500
         
     return {'success': True,
             'error': None,
             'message': 'List of table names found successfully',
             'data': list_of_table_names}, 200
+    
+def check_env_variables():
+    return os.getenv('HOST') is None or \
+        os.getenv('PORT') is None or \
+        os.getenv('DEBUG') is None or \
+        os.getenv('PATH_DB_DEFINITION_DIR') is None
+    
+with app.app_context():
+    load_model()
+
 
 
 # entry point for the application
 if __name__ == '__main__':
+    # load env variables
+    load_dotenv()
+    # check env variables are loaded
+    if check_env_variables():
+        print('Env variables are loaded, check .env file and try again')
+        exit(1)
+    
     # setting debug to True enables hot reload
-    app.debug = True
-    load_model()
-    app.run()
+    app.debug = os.getenv('DEBUG')
+    app.run(host=os.getenv('HOST'), port=os.getenv('PORT'))
--- microservice_helper.py
+++ microservice_helper.py
@@ -1,18 +1,30 @@
+import os
+import gensim.downloader as api
 import difflib
 import nltk
 import json
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-
+from collections import OrderedDict
+from flask import current_app
 
 nltk.download('stopwords')
 nltk.download('punkt')
 
+def load_model():
+    if not hasattr(current_app, 'word2vec_model'):
+        print("Loading model...")
+        current_app.word2vec_model = api.load('word2vec-google-news-300')
+        print("Model loaded successfully")
+
+# load the model of keywords previously trained (Google News)
+
+    
 # preprocess_text: Process the text to remove stopwords and punctuation marks.
 # Input: text
 # Output: text without stopwords and punctuation marks
 # Example: preprocess_text("I have reports") -> "have reports"words and punctuation marks.
-def preprocess_text(text): 
+def tokenize_text(text): 
     tokens = word_tokenize(text.lower()) # Tokenize and lowercase
 
     # Delete the stopwords and punctuation marks
@@ -22,61 +34,56 @@ def preprocess_text(text):
 
 
 # get_tables_related_to_the_question: Get the tables related to the question using difflib
-def get_tables_related_to_the_question(question):
-    processed_question = preprocess_text(question)
-    # get the tables names
-    table_names_list, error = get_list_of_table_names()
-    print (table_names_list)
+def get_tables_related_to_the_question(question, temperature=1):
+    tables_related = [] # List of tables related to the question
+    # Process the question to remove stopwords and punctuation marks.
+    tokenized_question = tokenize_text(question)
+    # Get the list of table names
+    tables_names, error = get_list_of_table_names()
     if error is not None:
-        print("Ups, something went wrong :(")
+        print("Oops, something went wrong :(")
         return None, error
     
-    best_matches = difflib.get_close_matches(processed_question, table_names_list, n=2, cutoff=0)
-    if len(best_matches) > 0:
-        print("Best match:", best_matches) # TODO
-        return get_simple_definitions(best_matches[0]), None
-    print("no match found", best_matches)
-    return None, None
+    # Get the similarity between the question and the table name
+    table_name_and_proximity_values = OrderedDict() 
+    for table_name in tables_names:
+        # get the similarity between the question and the table name
+        # proximity_rank is a float number between 0 and 1
+        proximity_value = current_app.word2vec_model.n_similarity(tokenized_question, table_name)
+        table_name_and_proximity_values[table_name] = proximity_value
+        print("Table:", table_name, "Ranking:", proximity_value)
+        if proximity_value > temperature:
+            tables_related.append(table_name)
 
-# read_json_file: Read JSON file with questions and answers and return a list of dictionaries
-def read_json_file():
-    try:
-        with open('db_definitions.json') as json_file:
-            data = json.load(json_file)
-            return data, None # Return data and None if there is no error
-    except Exception as error:
-        print("Error reading JSON file:", error)
-        return None, error # Return None and error if there is an error
+    # if there are tables related to the question, return them
+    if len(tables_related) > 0:
+        return tables_related, None
+    
+    # if there are no tables related to the question, return the table with the highest ranking
+    tables_related.append(max(table_name_and_proximity_values, key=table_name_and_proximity_values.get))
+    print("No tables related to the question, returning the table with the highest ranking"
+          , table_name_and_proximity_values)
+    return tables_related, None
 
 
-# get_list_of_table_names: Read JSON file with db_definitions and return a list of table names
+# get_list_of_table_names: Read all files in the directory and return a list of table names
 def get_list_of_table_names():
     table_names = []
-    # Read JSON file with db_definitions and return a list of table names
-    data, error = read_json_file() # Read JSON file with db_definitions
-    # check if there is an error reading the file
-    if error: # TODO: check if there is an error reading the file
-        return [], error # Return empty list and error if there is an error
-    # Get the list of table names
-    for schema in data['schemas']:
-        for table in schema['tables']:
-            table_names.append(table['name'])
-
+    for file in os.listdir(os.environ.get('PATH_DB_DEFINITION_DIR')):
+        if file.endswith(".txt"):
+            table_names.append(file.split('.')[0]) # Get the table name without extension
+            
     return table_names, None # Return list of table names and None if there is no error
 
 
-# get_simple_definitions: Read JSON file with db_definitions and return a definition of the table by name
+# get_simple_definitions: Read a file with db_definitions and return a definition of the table by name
 def get_simple_definitions(table_name):
-    # Read JSON file with db_definitions and return a definition of the table by name
-    data, error = read_json_file() # Read JSON file with db_definitions
-    # check if there is an error reading the file
-    if error:
-        return [], error # Return empty list and error if there is an error
-    # Get the list of table names
-    for schema in data['schemas']:
-        index = 0
-        for table in schema['tables']:
-            if table['name'] == table_name:
-                return schema['tables'][index], None # Return list of table names and None if there is no error
-            index += 1 # Increase index
-    return [], None # Return empty list and None if there is no error
+    try:
+        path_db_definitions = os.environ.get('PATH_DB_DEFINITION_DIR')
+        path_db_definitions = path_db_definitions + '/' + table_name + '.txt'
+        with open(path_db_definitions) as definitions_file:
+            definitions = definitions_file.read()
+            return definitions, None # Return definitions and None if there is no error
+    except Exception as error:
+        print("Error reading file:", error)
+        return None, error # Return None and error if there is an error
